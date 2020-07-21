d4bf15d5a566d192c500694be287b8dffdb688e4
sudhanshusethi2006 <sudhanshusethi2006@gmail.com>
2020-07-20 23:54:55 -0300

    adding seed method

---
+++ EshopApp/Data/Repositories/IPieRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EshopApp.Data.Entities;
+
+namespace EshopApp.Data.Repositories
+{
+    public interface IPieRepository
+    {
+        IEnumerable<Pie> GetAllPies();
+
+        Pie GetPieById(int pieId);
+    }
+}
---
+++ EshopApp/Data/Repositories/PieRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EshopApp.Data.Entities;
+
+namespace EshopApp.Data.Repositories
+{
+    public class PieRepository: IPieRepository
+    {
+        private readonly EshopContext _appDbContext;
+
+        public PieRepository(EshopContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public IEnumerable<Pie> GetAllPies()
+        {
+            return _appDbContext.Pies;
+        }
+
+        public Pie GetPieById(int pieId)
+        {
+            return _appDbContext.Pies.FirstOrDefault(p => p.Id == pieId);
+        }
+    }
+}
